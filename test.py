c38b3c5e28653561af4d048d565ad51adcaedcf8
niztg <65248634+niztg@users.noreply.github.com>
2020-09-29 17:07:16 -0400

    reee

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import dagpipy
+from secrets import token
 
-TOKEN = "wBNEsM746ixNkDdmgWYDTqQYZN2GDTzPgzlWUO58rl6rtO4JDAVgJ72zfFcfbpei"
-
-client = dagpipy.Client(TOKEN)
+client = dagpipy.Client(token)
 
 url = dagpipy.ImageURL("https://cdn.discordapp.com/avatars/731228973001867304/0e587564754286e061716f57713449b9.png?size=1024")
-whyareyougay = client.get_game(
-    dagpipy.Games.logo_guessing,
+whyareyougay = client.get_image(
+    dagpipy.ImageOptions.angel,
+    url=url
 )
-print(whyareyougay.difficulty)
+print(whyareyougay)
