b51ee4920609c4e525baec97189059922cb58af2
niztg <65248634+niztg@users.noreply.github.com>
2020-09-30 15:56:00 -0400

    token

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import dagpipy
+from secrets import TOKEN
 
-client = dagpipy.Client("aaaa")
+client = dagpipy.Client(TOKEN)
 
 url = dagpipy.ImageURL(
     "https://cdn.discordapp.com/avatars/731228973001867304/0e587564754286e061716f57713449b9.png?size=1024")
